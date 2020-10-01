d34f8f75a612b7bc18a10204f490f7061e59d798
DEEPAKMITTAL7 <dmittal9925.dm@gmail.com>
2020-10-01 17:20:34 +0530

    routing-done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { AdminmodifyuserComponent } from './adminmodifyuser/adminmodifyuser.component';
 
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmipayerComponent } from './emipayer/emipayer.component';
+import { EmitableComponent } from './emitable/emitable.component';
+import { FooterComponent } from './footer/footer.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { MeetTheTeamComponent } from './meet-the-team/meet-the-team.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { Product1Component } from './product1/product1.component';
+import { Product2Component } from './product2/product2.component';
+import { Product3Component } from './product3/product3.component';
+import { Product4Component } from './product4/product4.component';
+import { Product5Component } from './product5/product5.component';
+import { Product6Component } from './product6/product6.component';
+import { PurchasetableComponent } from './purchasetable/purchasetable.component';
 import { RegisterationComponent } from './registeration/registeration.component';
+import { UsernotactivatedComponent } from './usernotactivated/usernotactivated.component';
 
 const routes: Routes = [
   {path:'homepage', component: HomepageComponent},
@@ -15,7 +29,20 @@ const routes: Routes = [
   {path: 'registeration' , component: RegisterationComponent},
   {path: 'product-page' , component: ProductPageComponent},
   {path: 'dashboard' , component:DashboardComponent},
-  {path:'meet-the-team',component:MeetTheTeamComponent}
+  {path:'meet-the-team',component:MeetTheTeamComponent},
+  {path: 'emi-payer' , component:EmipayerComponent},
+  {path: 'emi-table', component:EmitableComponent},
+  {path:'footer', component:FooterComponent},
+  {path:'nav-bar', component:NavbarComponent},
+  {path:'product1',component:Product1Component},
+  {path:'product2',component:Product2Component},
+  {path:'product3',component:Product3Component},
+  {path:'product4',component:Product4Component},
+  {path:'product5',component:Product5Component},
+  {path:'product6',component:Product6Component},
+  {path:'purchase-table',component:PurchasetableComponent},
+  {path:'registeration',component:RegisterationComponent},
+  {path:'user-not-activated',component:UsernotactivatedComponent}
 ];
 
 @NgModule({
@@ -23,4 +50,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomepageComponent,LoginComponent,RegisterationComponent,ProductPageComponent,DashboardComponent]
+export const routingComponents = [HomepageComponent,LoginComponent,RegisterationComponent,ProductPageComponent,DashboardComponent, HomepageComponent,
+  LoginComponent,RegisterationComponent,ProductPageComponent,DashboardComponent,MeetTheTeamComponent,Product1Component,
+  Product2Component,Product3Component, Product4Component,Product5Component,
+  Product6Component, AdminhomeComponent,AdminmodifyuserComponent,EmipayerComponent,PurchasetableComponent,EmitableComponent, NavbarComponent,
+  FooterComponent, UsernotactivatedComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,51 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterationComponent } from './registeration/registeration.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MeetTheTeamComponent } from './meet-the-team/meet-the-team.component';
-import { Product1Component } from './product1/product1.component';
-import { Product2Component } from './product2/product2.component';
-import { Product3Component } from './product3/product3.component';
-import { Product4Component } from './product4/product4.component';
-import { Product5Component } from './product5/product5.component';
-import { Product6Component } from './product6/product6.component';
-import { AdminhomeComponent } from './adminhome/adminhome.component';
-import { AdminmodifyuserComponent } from './adminmodifyuser/adminmodifyuser.component';
-import { EmipayerComponent } from './emipayer/emipayer.component';
-import { PurchasetableComponent } from './purchasetable/purchasetable.component';
-import { EmitableComponent } from './emitable/emitable.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { UsernotactivatedComponent } from './usernotactivated/usernotactivated.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-  routingComponents,
-  HomepageComponent,
-  LoginComponent,
-  RegisterationComponent,
-  ProductPageComponent,
-  DashboardComponent,
-  MeetTheTeamComponent,
-  Product1Component,
-  Product2Component,
-  Product3Component,
-  Product4Component,
-  Product5Component,
-  Product6Component,
-  AdminhomeComponent,
-  AdminmodifyuserComponent,
-  EmipayerComponent,
-  PurchasetableComponent,
-  EmitableComponent,
-  NavbarComponent,
-  FooterComponent,
-  UsernotactivatedComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
